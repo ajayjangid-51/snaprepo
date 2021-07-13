b91d4645edb6e69a3ecd5b7050b99b6763e67038
AjayJangid511 <akjjan7725@gmail.com>
2021-07-13 18:40:32 +0530

    Updates

---
+++ precache-manifest.7670d60d5fd4a86c960d0a7ccd65a58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556d145a67f3046883eb744ab0488dde",
+    "url": "/snaprepo/index.html"
+  },
+  {
+    "revision": "1a9bad0ac6f0d29fa8fc",
+    "url": "/snaprepo/static/css/main.ec41352a.chunk.css"
+  },
+  {
+    "revision": "b6228b7f85760377d592",
+    "url": "/snaprepo/static/js/2.beea703f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/snaprepo/static/js/2.beea703f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9bad0ac6f0d29fa8fc",
+    "url": "/snaprepo/static/js/main.1ec4eab8.chunk.js"
+  },
+  {
+    "revision": "1fc173436f0e7af7ed8c",
+    "url": "/snaprepo/static/js/runtime-main.9980cf89.js"
+  }
+]);
